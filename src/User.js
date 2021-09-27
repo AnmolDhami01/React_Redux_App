b16741880a837d85164a643b343c2fa23379a3dd
Anmol Dhami <anmoldhami01@gmail.com>
2021-09-27 22:58:26 +0530

    Rect Redux App

---
+++ src/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const User = (props) => {
+const {data} = props
+    return (
+        <div>
+            <h1>User Component</h1>
+            <h1>{data.name}</h1>
+            <h1>{data.age}</h1>
+        </div>
+    );
+};
+
+export default User;
